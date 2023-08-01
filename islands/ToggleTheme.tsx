80d3688ea0b27a81b48d72e8665803aecdc7149e
andrew1412 <unserhumanomas@proton.me>
2023-07-31 22:10:19 -0400

    modified:   islands/ToggleTheme.tsx

--- islands/ToggleTheme.tsx
+++ islands/ToggleTheme.tsx
@@ -1,12 +1,19 @@
 import { Moon, Sun } from "preact-feather";
 import { useEffect, useState } from "preact/hooks";
 
-function Changer1() {
-  const [isDark, setIsDark] = useState(true);
+export default function Changer() {
+  const [isDark, setIsDark] = useState(() => {
+    const savedTheme = window.localStorage.getItem("theme");
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", JSON.stringify(isDark));
+  }, [isDark]);
+
   return (
     <>
       <span
-        data-toggle-theme="dark, light"
         className="cursor-pointer"
         onClick={() => setIsDark(!isDark)}
       >
@@ -32,7 +39,7 @@ function Changer1() {
   );
 }
 
-export default function Changer() {
+function Changer1() {
   const [isDark, setIsDark] = useState(() => {
     // Get the current theme from localStorage or default to 'light'
     const savedTheme = window.localStorage.getItem("theme");
@@ -65,46 +72,3 @@ export default function Changer() {
     </button>
   );
 }
-
-const DarkMode = () => {
-  let clickedClass = "clicked";
-  const body = document.body;
-  const lightTheme = "light";
-  const darkTheme = "dark";
-  let theme: string | null;
-
-  if (localStorage) {
-    theme = localStorage.getItem("theme");
-  }
-
-  if (theme === lightTheme || theme === darkTheme) {
-    body.classList.add(theme);
-  } else {
-    body.classList.add(lightTheme);
-  }
-
-  const switchTheme = (e) => {
-    if (theme === darkTheme) {
-      body.classList.replace(darkTheme, lightTheme);
-      e.target.classList.remove(clickedClass);
-      localStorage.setItem("theme", "light");
-      theme = lightTheme;
-    } else {
-      body.classList.replace(lightTheme, darkTheme);
-      e.target.classList.add(clickedClass);
-      localStorage.setItem("theme", "dark");
-      theme = darkTheme;
-    }
-  };
-
-  return (
-    <button
-      className={theme === "dark" ? clickedClass : ""}
-      id="darkMode"
-      onClick={(e) => switchTheme(e)}
-    >
-    </button>
-  );
-};
-
-//export default DarkMode;
