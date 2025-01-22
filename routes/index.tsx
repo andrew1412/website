5161c91a6a7f4d7d6b459decfe09a247cd35c0ad
Andrew <unserhumanomas@proton.me>
2025-01-22 04:11:42 +0000

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -108,15 +108,9 @@ export default function Home({ data }: PageProps<HandlerProps | null>) {
                   title={banner.title}
                   text={banner.text}
                 />
-              )}
-            <div class="flex flex-col items-center w-full w-full rounded-xl p-4">
-              <a 
-              href="https://www.buymeacoffee.com/andrew1412">
-              <img 
-                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrew1412&button_colour=FFDD00&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=ffffff" />
-              </a>
-            </div>  
+              )} 
               <TabsIsland links={links} feed={feed} />
+              <a href="https://www.buymeacoffee.com/andrew1412"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrew1412&button_colour=FFDD00&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=ffffff" /></a>
             </div>
           </div>
         </main>
