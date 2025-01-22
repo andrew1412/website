d8cd94eec11e57418711b4808b356f58c3fd3877
Andrew <unserhumanomas@proton.me>
2025-01-22 05:25:55 +0000

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -118,6 +118,7 @@ export default function Home({ data }: PageProps<HandlerProps | null>) {
                 >
               <img 
               src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrew1412&button_colour=FFDD00&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=ffffff" 
+              class="flex justify-between bg-metal rounded-lg px-6 py-3 w-full items-center text-white"
               />
              </a>
           </div>
