1ee9db1e53d167a8a3f83bad30ebc45179cf4600
Andrew <unserhumanomas@proton.me>
2025-01-22 06:10:28 +0000

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,19 +6,21 @@ export default function Banner(
   const { title, text } = props;
 
   return (
-    <div class="rounded-2xl px-4 py-4 mb-4 min-w-full bg-blue">
-      <div class="flex space-x-4">
-        <a 
-     href="https://www.buymeacoffee.com/andrew1412"
-     class="flex flex-wrap w-full mt-2"
-     target="_blank"
-     rel="noopener noreferrer"
+    <div>
+     <a 
+      href="https://www.buymeacoffee.com/andrew1412"
+      class="flex flex-wrap w-full mt-2"
+      target="_blank"
+      rel="noopener noreferrer"
      >
      <img 
-     src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrew1412&button_colour=5F7FFF&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" 
-     class="flex w-full"
+      src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrew1412&button_colour=5F7FFF&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" 
+      class="flex w-full"
      />
-    </a>
+     </a>
+    </div> 
+    <div class="rounded-2xl px-4 py-4 mb-4 min-w-full bg-blue">
+      <div class="flex space-x-4">
         <div>
           <Star size={20} color={"#89b5fa"} />
         </div>
