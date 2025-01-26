30917863b63a08c6d09cb9de988d1530d9a2bb15
Andrew <unserhumanomas@proton.me>
2025-01-26 23:24:34 +0000

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,32 +7,21 @@ export default function Banner(
 
   return (
     <div class="rounded-2xl px-4 py-4 mb-4 min-w-full bg-blue">
-      <a 
-      href="href="https://liberapay.com/Andrew1412/donate"
-      class="flex flex-wrap w-full"
-      target="_blank"
-      rel="noopener noreferrer"
-     >
-     <img 
-      src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrew1412&button_colour=374052&font_colour=c9d1d9&font_family=Lato&outline_colour=c9d1d9&coffee_colour=89b5fa" 
-      class="flex w-full"
-     />
-     </a>
-      <a href="https://streamelements.com/andrew1412/tip">
-      <div class="flex space-x-4">
-        <div>
-          <Star size={20} color={"#89b5fa"} />
-        </div>
-        <div>
-          <h2 class="font-bold leading-tight mb-1 text-white">
-            {title}
-          </h2>
-          <p class="text-sm text-silver">
-            {text}
-          </p>
-        </div>
-      </div>
-        </a>
+      <a href="https://streamelements.com/andrew1412/tip" target="_blank" rel="noopener noreferrer">
+       <div class="flex space-x-4">
+         <div>
+           <Star size={20} color={"#89b5fa"} />
+         </div>
+         <div>
+           <h2 class="font-bold leading-tight mb-1 text-white">
+             {title}
+           </h2>
+           <p class="text-sm text-silver">
+             {text}
+           </p>
+         </div>
+       </div>
+      </a>
     </div>
   );
 }
