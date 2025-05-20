f733904bb43578cf98bf9f0d97d083e74695fade
Andrew <unserhumanomas@proton.me>
2025-05-20 18:29:28 -0400

    Update ReadmeButton.tsx

--- components/ReadmeButton.tsx
+++ components/ReadmeButton.tsx
@@ -4,7 +4,7 @@ export default function ReadmeButton() {
       class="mb-4 font-mono bg-metal font-bold text-sm py-1 px-4 rounded-lg cursor-pointer mt-2 text-white"
       href="/"
     >
-      Inicio
+      Home
     </a>
   );
 }
